8650f921cd9787a53552f7eed5a441c719063150
Ketan Goyal <ketangoyal1988@gmail.com>
2019-10-12 15:27:30 -0400

    Providers done. PDP unit test in-progress
    TODO:
    1. PDP unit tests
    2. Memory storage
    3. File storage
    4. SQL storage

--- tests/test_context.py
+++ tests/test_context.py
@@ -5,8 +5,16 @@
 import pytest
 
 from py_abac.context import EvaluationContext
-from py_abac.request import Request
 from py_abac.exceptions import InvalidAccessControlElementError, InvalidAttributePathError
+from py_abac.provider.base import AttributeProvider
+from py_abac.request import Request
+
+
+class EmailAttributeProvider(AttributeProvider):
+
+    def get_attribute_value(self, ace: str, attribute_path: str):
+        if ace == "subject" and attribute_path == "$.email":
+            return "carl@gmail.com"
 
 
 def test_create():
@@ -35,6 +43,7 @@ def test_create():
     assert context.subject_id == request._subject_id
     assert context.resource_id == request._resource_id
     assert context.action_id == request._action_id
+    assert context._other_providers == []
     assert context.ace is None
     assert context.attribute_path is None
 
@@ -47,6 +56,35 @@ def test_create():
     context.ace = "resource"
     context.attribute_path = "$.name"
     assert context.attribute_value == "Calendar"
+
+
+def test_get_attribute_value():
+    request_json = {
+        "subject": {
+            "id": "a",
+            "attributes": {
+                "firstName": "Carl",
+                "lastName": "Right"
+            }
+        },
+        "resource": {
+            "id": "a",
+            "attributes": {
+                "name": "Calendar"
+            }
+        },
+        "action": {
+            "id": "",
+            "attributes": {}
+        },
+        "context": {}
+    }
+    request = Request.from_json(request_json)
+    context = EvaluationContext(request, providers=[EmailAttributeProvider()])
+    assert context.get_attribute_value("subject", "$.firstName") == "Carl"
+    assert context.get_attribute_value("subject", "$.email") == "carl@gmail.com"
+    assert context.get_attribute_value("resource", "$.name") == "Calendar"
+    assert context.get_attribute_value("context", "$.ip") is None
 
 
 def test_attribute_value_raises():
