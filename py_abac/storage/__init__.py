8ef9f7e3637bf28e3603854ca032cd96eb75aba1
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-08-04 09:24:08 -0700

    allow for only one storage extra

--- py_abac/storage/__init__.py
+++ py_abac/storage/__init__.py
@@ -2,5 +2,20 @@
     Exposed classes and methods
 """
 
-from .mongo import MongoStorage, MongoMigrationSet
-from .sql import SQLStorage, SQLMigrationSet
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    from .mongo import MongoStorage, MongoMigrationSet
+    logger.info('mongo storage imported')  
+except ImportError:
+    # failure to import may mean that only sql extras are installed
+    pass
+
+try:
+    from .sql import SQLStorage, SQLMigrationSet
+    logger.info('sql storage imported')  
+except ImportError:
+    # failure to import may mean that only mongo extras are installed
+    pass
