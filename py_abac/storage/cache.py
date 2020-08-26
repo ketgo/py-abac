9538d0a163baef8d6991e88b888ad0474d8aab94
ketgo <ketangoyal1988@gmail.com>
2020-08-25 22:10:14 -0400

    Updated storage cache.py

--- py_abac/storage/cache.py
+++ py_abac/storage/cache.py
@@ -2,10 +2,14 @@
     Cache for policy storage
 """
 
+import logging
 from typing import Union, Generator
 
 from .base import Storage
+from .memory import MemoryStorage
 from ..policy import Policy
+
+LOG = logging.getLogger(__name__)
 
 
 class StorageCache(Storage):
@@ -16,31 +20,41 @@ class StorageCache(Storage):
 
         .. code-block:: python
 
+            from py_abac.storage.memory
+
         :param storage: policy storage instance.
-        :param max_size: maximum size of the cache. The size
-            represents number of policies stored.
+        :param cache: policy storage instance acting as cache. Default is set
+            to MemoryStorage with `max_size` of 1024.
     """
 
-    def __init__(self, storage: Storage, max_size: int = 1024):
+    def __init__(self, storage: Storage, cache: Storage = MemoryStorage(max_size=1024)):
         self._storage = storage
-        self._max_size = max_size
+        self._cache = cache
 
     def add(self, policy: Policy):
         """
             Store a policy
         """
-        raise NotImplementedError()
+        self._storage.add(policy)
+        self._cache.add(policy)
 
     def get(self, uid: str) -> Union[Policy, None]:
         """
             Get specific policy
         """
-        raise NotImplementedError()
+        policy = self._cache.get(uid)
+        if not policy:
+            LOG.debug("Cache miss occurred. Looking policy from storage.")
+            policy = self._storage.get(uid)
+
+        return policy
 
     def get_all(self, limit: int, offset: int) -> Generator[Policy, None, None]:
         """
             Retrieve all the policies within a window
         """
+        # TODO: Sort policies by UID for all storage. This will allow utilization
+        #  of any partial results from cache.
         raise NotImplementedError()
 
     def get_for_target(
@@ -52,16 +66,23 @@ class StorageCache(Storage):
         """
             Get all policies for given target IDs.
         """
+        # TODO:
+        #  1. Needs a separate cache which indicates if the query is to be run on
+        #  cache or backend storage.
+        #  2. Need a mechanism to avoid duplicating policy objects in memory since
+        #  another cache is introduced. Maybe store only policy UID in the new cache
         raise NotImplementedError()
 
     def update(self, policy: Policy):
         """
             Update a policy
         """
-        raise NotImplementedError()
+        self._storage.update(policy)
+        self._cache.update(policy)
 
     def delete(self, uid: str):
         """
             Delete a policy
         """
-        raise NotImplementedError()
+        self._storage.delete(uid)
+        self._cache.delete(uid)
